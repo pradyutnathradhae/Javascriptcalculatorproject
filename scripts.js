999d3af1a678e346c55045456db64fdf5f447c11
pradyutnathradhae <pradyutvam@protonmail.com>
2019-07-22 16:17:16 +0530

    Finalizing script funcionalities

--- scripts.js
+++ scripts.js
@@ -16,7 +16,62 @@ function printOutput(num){
   }
 }
 function getFormattedText(num){
+  if(num === '-'){
+    return "";
+  }
   var n = Number(num);
   var value = n.toLocaleString("en");
   return value;
+}
+function reverseNumberFormat(num){
+  return Number(num.replace(/,/g,''));
+}
+var operator = document.getElementsByClassName('operator');
+for(var i=0;i<operator.length;i++){
+  operator[i].addEventListener("click",function(){
+      if(this.id === 'clear'){
+        printHistory("");
+        printOutput("");
+      }
+      else if (this.id === 'backspace') {
+        var output = reverseNumberFormat(getOutput()).toString();
+        if(output){//check if output has a value
+          output = output.substr(0,output.length - 1);
+          printOutput(output);
+        }
+      }
+      else{
+        var output1 = getOutput();
+        var history = getHistory();
+        if(output1 === "" && history !== ""){
+          if(isNaN(history[history.length - 1])){
+            history = history.substr(0,history.length - 1);
+          }
+          if(output1 !== "" || history !== ""){
+            output1 = output1 === ""?output1:reverseNumberFormat(output1);
+            history = history + output1;
+            if(this.id === "="){
+              var result = eval(history);
+              printHistory("");
+              printOutput(result);
+            }
+            else{
+              history = history + this.id;
+              printHistory(history);
+              printOutput("");
+            }
+          }
+        }
+      }
+  });
+}
+var number = document.getElementsByClassName('number');
+for(var i=0;i<number.length;i++){
+  number[i].addEventListener("click",function(){
+      var output = reverseNumberFormat(getOutput());
+      if(!isNaN(output)){//check if prevoutput format is number
+        output = output + this.id;
+        printOutput(output);
+      }
+  });
 }
